0bb112fa0a4e908ab5a6f5d7086a2c51e1f76b85
Udara Rathnasooriya <bika.learning@gmail.com>
2025-09-08 23:00:23 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import About from "./pages/About";
+import Header from "./compoennts/Header";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+      <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ src/compoennts/Header.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const navigation = [
+  { name: "Home", to: "/" },
+  { name: "About", to: "/about" },
+ 
+];
+
+const Header = () => {
+  return (
+    <header className="bg-indigo-600">
+      <nav aria-label="Top" className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="flex w-full items-center justify-between border-b border-indigo-500 py-6 lg:border-none">
+          <div className="flex items-center">
+            <a href="#">
+              <span className="sr-only">Your Company</span>
+              <img
+                alt=""
+                src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=white"
+                className="h-10 w-auto"
+              />
+            </a>
+            <div className="ml-10 hidden space-x-8 lg:block">
+              {navigation.map((link) => (
+                <Link
+                  key={link.name}
+                  to={link.to}
+                  className="text-base font-medium text-white hover:text-indigo-50">
+                  {link.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="ml-10 space-x-4">
+            <Link
+              to="/login"
+              className="inline-block rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-base font-medium text-white hover:bg-indigo-500/75">
+              Sign in
+            </Link>
+            <Link
+              to="/register"
+              className="inline-block rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-indigo-600 hover:bg-indigo-50">
+              Sign up
+            </Link>
+          </div>
+        </div>
+        <div className="flex flex-wrap justify-center gap-x-6 py-4 lg:hidden">
+          {navigation.map((link) => (
+            <a
+              key={link.name}
+              href={link.href}
+              className="text-base font-medium text-white hover:text-indigo-50">
+              {link.name}
+            </a>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
